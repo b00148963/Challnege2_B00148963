9d20db42337eb7b85cc3ca9bfbf2a549b87f1574
b00148963 <b00148963@mytudublin.ie>
2022-10-14 15:16:30 +0100

    Initial commit

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float slowdown = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && slowdown <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            slowdown = 1;
+        }
+        if (slowdown >= 0)
+        {
+            slowdown -= Time.deltaTime;
+        }
+    }
+}
